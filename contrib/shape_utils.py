c41d1a635cd2ac95bfb44314157425c9d76645da
delta37 <delta37@delta37.delta37>
2017-10-03 01:47:07 +0300

    shape_utils: deduce shape

--- contrib/shape_utils.py
+++ contrib/shape_utils.py
@@ -1,4 +1,11 @@
 import torch.nn as nn
 import torch
+from base.layers import Layer
+from torch.autograd import Variable
 
-def ShapeOutput(in_shape, func)
+def ShapeOutput(in_shape, layer_name, *args, **kwargs):
+    x = Variable(torch.randn(*in_shape))
+    f = Layer.AccessableMethods[layer_name](*args, **kwargs)
+    y = f(x)
+    return y.data.numpy().shape
+
