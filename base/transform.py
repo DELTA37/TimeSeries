dd2a55b47e14ec4948f4e5628de4a53684f8e1c4
delta37 <delta37@delta37.delta37>
2017-10-06 01:34:01 +0300

    kalman filter

--- base/transform.py
+++ base/transform.py
@@ -13,4 +13,16 @@ def RollingWindow(x, window, dilated_rate=0, stride=1, b=0, padding=0):
             res.append(np.sum(x[0: i * stride - b + 1] * window[len(window) + b - 1 - i * stride:]))
     return np.array(res)
     
-    
+def getKalmanFilter(sig_ksi, sig_eta, z0): 
+    def kalman(z_k1, u_k):
+        kalman.Ee = (kalman.sig_eta**2) * (kalman.Ee + kalman.sig_ksi**2) / (kalman.Ee + kalman.sig_eta**2 + kalman.sig_ksi**2)
+        kalman.K = kalman.Ee / kalman.sig_eta ** 2
+        kalman.x = kalman.K * z_k1 + (1 - kalman.K) * (kalman.x + u_k)
+
+        return kalman.x
+    kalman.Ee = sig_eta ** 2
+    kalman.x = z0
+    kalman.K = 0
+    return kalman
+
+
