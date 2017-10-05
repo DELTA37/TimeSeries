eef770102b78b056e5c95f015f9669b7a5a0878f
delta37 <delta37@delta37.delta37>
2017-10-05 22:27:11 +0300

    xml2pandas

---
+++ moex/parser.py
@@ -0,0 +1,73 @@
+import requests
+import pandas as pd
+import xml.etree.ElementTree as ET
+from datetime import datetime
+import numpy as np
+
+class Parser:
+    @staticmethod
+    def type_correct(typename):
+        if typename == 'string':
+            return 'str'
+        elif typename == 'date':
+            return 'datetime64[ns]'
+        else:
+            return typename
+
+    @staticmethod
+    def obj_correct(obj, typename):
+        if typename == 'datetime64[ns]':
+            return pd.to_datetime(obj)
+        elif typename == 'str':
+            return str(obj)
+        elif typename == 'int64':
+            try:
+                return np.int64(obj)
+            except:
+                return np.nan
+        elif typename == 'int32':
+            try:
+                return np.int32(obj)
+            except:
+                return np.nan
+        else:
+            return obj
+
+    def __init__(self, params):
+        pass
+    
+    def xml2pandas(self, doc):
+        root = ET.fromstring(doc)
+        assert(root.tag == 'document')
+        tables = root.getchildren()
+        dfs = []
+        for table in tables:
+            assert(table.tag == 'data')
+            name        = table.attrib['id']
+
+            child       = table.getchildren()
+            metadata    = child[0]
+            assert(metadata.tag == 'metadata')
+            rows        = child[1]
+            assert(rows.tag == 'rows')
+            
+            columns     = metadata.getchildren()[0]
+            assert(columns.tag == 'columns')
+            
+            col_types = dict()
+            col_names = []
+            for col in columns.getchildren():
+                col_names.append(col.attrib['name'])
+                col_types[col.attrib['name']] = Parser.type_correct(col.attrib['type'])
+
+            df = pd.DataFrame(columns=col_names).astype(col_types)
+             
+            for row in rows.getchildren():
+                row_attr = {}
+                for name in col_names:
+                    row_attr[name] = Parser.obj_correct(row.attrib[name], col_types[name])
+                row_attr = pd.DataFrame(row_attr, index=[0])
+                df = df.append(pd.DataFrame(row_attr, columns=col_names), ignore_index=True)
+            dfs.append(df)
+        return dfs
+        
