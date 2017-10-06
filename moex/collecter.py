ea235b1c945cb554f21af93cf28ae43ecb078b41
delta37 <delta37@delta37.delta37>
2017-10-06 12:39:18 +0300

    splitit

--- moex/collecter.py
+++ moex/collecter.py
@@ -1,28 +1,40 @@
 import moex.parser
 import matplotlib.pyplot as plt
 import os
+import pandas as pd
 
 def getPriceArray(security):
     if not hasattr(getPriceArray, "parser"):
         getPriceArray.parser = moex.parser.Parser({})
     
+    def to_float(x, dtype):
+        if str.isdigit(x.replace('.','',1)):
+            return eval(dtype)(x)
+        else:
+            return 0
     engine, market = getPriceArray.parser.getEngineMarketForSecurity(security)
     inf = getPriceArray.parser.iss_history(engine, market, security)[0]
 
-    VALUE                       = inf['VALUE'].fillna(0) 
+#    VALUE                       = inf['VALUE'].fillna(0) 
+    inf['OPEN']                 = pd.to_numeric(inf['OPEN'], errors='coerce')
     OPEN                        = inf['OPEN'].fillna(0)
+
+    inf['LOW']                  = pd.to_numeric(inf['LOW'], errors='coerce')
     LOW                         = inf['LOW'].fillna(0)
+
+    inf['HIGH']                 = pd.to_numeric(inf['HIGH'], errors='coerce')
     HIGH                        = inf['HIGH'].fillna(0)
-    LEGALCLOSEPRICE             = inf['LEGALCLOSEPRICE'].fillna(0)
-    WAPRICE                     = inf['WAPRICE'].fillna(0)
+#    LEGALCLOSEPRICE             = inf['LEGALCLOSEPRICE'].fillna(0)
+#    WAPRICE                     = inf['WAPRICE'].fillna(0)
+    inf['CLOSE']                = pd.to_numeric(inf['CLOSE'], errors='coerce')
     CLOSE                       = inf['CLOSE'].fillna(0)
-    VOLUME                      = inf['VOLUME'].fillna(0)
-    MARKETPRICE2                = inf['MARKETPRICE2'].fillna(0)
-    MARKETPRICE3                = inf['MARKETPRICE3'].fillna(0)
-    ADMITTEDQUOTE               = inf['ADMITTEDQUOTE'].fillna(0)
-    MP2VALTRD                   = inf['MP2VALTRD'].fillna(0)
-    MARKETPRICE3TRADESVALUE     = inf['MARKETPRICE3TRADESVALUE'].fillna(0)
-    ADMITTEDVALUE               = inf['ADMITTEDVALUE'].fillna(0)
+#    VOLUME                      = inf['VOLUME'].fillna(0)
+#    MARKETPRICE2                = inf['MARKETPRICE2'].fillna(0)
+#    MARKETPRICE3                = inf['MARKETPRICE3'].fillna(0)
+#    ADMITTEDQUOTE               = inf['ADMITTEDQUOTE'].fillna(0)
+#    MP2VALTRD                   = inf['MP2VALTRD'].fillna(0)
+#    MARKETPRICE3TRADESVALUE     = inf['MARKETPRICE3TRADESVALUE'].fillna(0)
+#    ADMITTEDVALUE               = inf['ADMITTEDVALUE'].fillna(0)
     TRADEDATE                   = inf['TRADEDATE']
 
     return {'OPEN' : OPEN, 'HIGH' : HIGH, 'LOW' : LOW, 'CLOSE' : CLOSE, 'DATE' : TRADEDATE}
@@ -30,14 +42,19 @@ def getPriceArray(security):
 def PlotData(security, path, splitit = False):
     d = getPriceArray(security)
     if splitit:
+        d['OPEN'].to_csv('1.txt')
         plt.plot(d['OPEN'], label='OPEN', color='b')
         plt.savefig(os.path.join(path, 'OPEN_' + security + '.png'))
+        plt.clf()
         plt.plot(d['CLOSE'], label='CLOSE', color='g')
         plt.savefig(os.path.join(path, 'CLOSE_' + security + '.png'))
+        plt.clf()
         plt.plot(d['HIGH'], label='HIGH', color='r')
         plt.savefig(os.path.join(path, 'HIGH_' + security + '.png'))
+        plt.clf()
         plt.plot(d['LOW'], label='LOW', color='k')
         plt.savefig(os.path.join(path, 'LOW_' + security + '.png'))
+        plt.clf()
     else:
         plt.plot(d['OPEN'], label='OPEN', color='b')
         plt.plot(d['CLOSE'], label='CLOSE', color='g')
--- plotter.py
+++ plotter.py
@@ -4,13 +4,18 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('security', type=str)
 parser.add_argument('--path', type=str)
+parser.add_argument('--splitit', type=int)
 
 args = parser.parse_args()
 
 security = args.security
+splitit = args.splitit
+if splitit == None:
+    splitit = 0
+
 if args.path == None:
     path = './'
 else:
     path = args.path
 
-moex.collecter.PlotData(security, path)
+moex.collecter.PlotData(security, path, splitit)
