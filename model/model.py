86c1eccac2a9ad654e8c015e82007fee350ad886
delta37 <delta37@delta37.delta37>
2017-11-27 20:55:36 +0300

    dual learning model

--- model/model.py
+++ model/model.py
@@ -1,6 +1,7 @@
 import argparse
 import torch
 import torch.nn as nn
+from torch.optim import RMSprop
 from collections import OrderedDict
 from torch.autograd import Variable
 import torch.nn.functional as F
@@ -12,8 +13,6 @@ class Net(BaseNet):
     '''
     @info             : this place is for global parameter (hyperparameters) definition
     '''
-    IMAGE_SHAPE = [3, 3 * 32, 3 * 32]
-    LABEL_SHAPE = [1]
     def __init__(self, params, *args, **kwargs):
         super(Net, self).__init__(params, *args, **kwargs)
         '''
@@ -25,26 +24,39 @@ class Net(BaseNet):
         @return       : constructor
         @rtype        : None
         '''
-        self.layer11 = Conv2d_BN_ReLULayer(3, 64, 3)
-        self.layer12 = Conv2d_BN_ReLULayer(64, 64, 3)
-        self.pool1  = MaxPool2dLayer(2)
-        self.layer21 = Conv2d_BN_ReLULayer(64, 128, 3)
-        self.layer22 = Conv2d_BN_ReLULayer(128, 128, 3)
-        self.layer23 = Conv2d_BN_ReLULayer(128, 128, 3)
-        self.pool2  = MaxPool2dLayer(2)
-        self.layer31 = Conv2d_BN_ReLULayer(128, 256, 3)
-        self.layer32 = Conv2d_BN_ReLULayer(256, 256, 3)
-        self.layer33 = Conv2d_BN_ReLULayer(256, 256, 3)
-        self.pool3  = MaxPool2dLayer(2)
-        self.layer41 = Conv2d_BN_ReLULayer(256, 512, 3)
-        self.layer42 = Conv2d_BN_ReLULayer(512, 512, 3)
-        self.layer43 = Conv2d_BN_ReLULayer(512, 512, 3)
-        self.pool4  = MaxPool2dLayer(2)
-        self.fclayer1 = Conv2dLayer(512, 9 * 512, 3, stride=2)
-        self.fclayer2 = Conv2dLayer(9 * 512, 9 * 512, 1)
-        self.fclayer3 = Conv2dLayer(9 * 512, 1, 1)
-        self.sig = SigmoidLayer()
-     
+        self.X_dim = params['X_dim']
+        self.z_dim = params['z_dim']
+        self.h_dim = params['h_dim']
+        self.lam1  = params['lam1']
+        self.lam2  = params['lam2']
+
+        self.G1 = SequentialBlock(
+                LinearLayer(self.X_dim + self.z_dim, self.h_dim),
+                ReLULayer(),
+                LinearLayer(self.h_dim, self.X_dim),
+                SigmoidLayer(),
+        )
+
+        self.G2 = SequentialBlock(
+                LinearLayer(self.X_dim + self.z_dim, self.h_dim),
+                ReLULayer(),
+                LinearLayer(self.h_dim, self.X_dim),
+                SigmoidLayer(),
+        )
+
+        self.D1 = SequentialBlock(
+            LinearLayer(self.X_dim, self.h_dim),
+            ReLULayer(),
+            LinearLayer(self.h_dim, 1),
+        )
+
+        self.D2 = SequentialBlock(
+            LinearLayer(self.X_dim, self.h_dim),
+            ReLULayer(),
+            LinearLayer(self.h_dim, 1),
+        )
+
+
     def get_inputs(self):
         '''
         @info         : this function is for defining inputs of model, there names and shapes
@@ -52,7 +64,12 @@ class Net(BaseNet):
         @return       : name maped to variable which contain inside a tensor needed shape
         @rtype        : dict(str : torch.autograd.Variable(torch.FloatTensor(), requires_grad=False))  
         '''
-        return {'image' : Variable(torch.randn(*([self.params['batch_size']] + Net.IMAGE_SHAPE)), requires_grad=False)}
+        return {
+            'X1' : Variable(torch.randn(*([self.params['batch_size']] + [self.X_dim])), requires_grad=False), 
+            'X2' : Variable(torch.randn(*([self.params['batch_size']] + [self.X_dim])), requires_grad=False), 
+            'z1' : Variable(torch.randn(*([self.params['batch_size']] + [self.z_dim])), requires_grad=False), 
+            'z2' : Variable(torch.randn(*([self.params['batch_size']] + [self.z_dim])), requires_grad=False), 
+        }
    
 
     def dict_forward(self, x):
@@ -65,28 +82,30 @@ class Net(BaseNet):
         @return       : output of our model after all operations
         @rtype        : dict(str : torch.autograd.Variable())
         '''
-        x = self.layer11(x['image'])
-        x = self.layer12(x)
-        x = self.pool1(x)
-        print("step 1")
-        x = self.layer21(x)
-        x = self.layer22(x)
-        x = self.pool2(x)
-        print("step 2")
-        x = self.layer31(x)
-        x = self.layer32(x)
-        x = self.pool3(x)
-        print("step 3")
-        x = self.layer41(x)
-        x = self.layer42(x)
-        x = self.pool4(x)
-        print("step 4")
-        x = self.fclayer1(x)
-        x = self.fclayer2(x)
-        x = self.fclayer3(x)
-        x = self.sig(x)
-        x = x.view(-1)
-        return {'label' : x}
+
+        G1_X1 = self.G1(torch.cat([x['X1'], x['z1']], 1))
+        D1_real = self.D1(x['X2'])
+        D1_fake = self.D1(G1_X1)
+
+        G2_X2 = self.G2(torch.cat([x['X2'], x['z2']], 1))
+        D2_real = self.D2(x['X2'])
+        D2_fake = self.D2(G2_X2)
+
+        G2_G1_X1 = self.G2(G1_X1)
+        G1_G2_X2 = self.G1(G2_X2)
+
+        return {
+            'X1'      : x['X1'],
+            'D1_real' : D1_real,
+            'D1_fake' : D1_fake,
+
+            'X2'      : x['X2'],
+            'D2_real' : D2_real,
+            'D2_fake' : D2_fake,
+            
+            'X1_recon': G2_G1_X1,
+            'X2_recon': G1_G2_X2,
+        }
 
     def get_criterion(self, params):
         '''
@@ -101,12 +120,35 @@ class Net(BaseNet):
         class Loss(nn.Module):
             def __init__(self):
                 super(Loss, self).__init__()
-                self.aggr_loss = torch.nn.NLLLoss() # you can provide your own loss function
             def forward(self, y_pred, y):
-                return self.aggr_loss.forward(y_pred['label'], y['label']) # for a dict values passed to __call__
-            def backward(self):
-                return self.aggr_loss.backward()
+                loss = -(torch.mean(y_pred['D1_real']) - torch.mean(y_pred['D1_fake']))
+                loss -= (torch.mean(y_pred['D2_real']) - torch.mean(y_pred['D2_fake']))
+                loss -= torch.mean(y_pred['D1_fake']) + torch.mean(y_pred['D2_fake'])
+                loss += self.lam1 * torch.mean(torch.sum(torch.abs(y_pred['X1_recon'] - y_pred['X1']), 1))
+                loss += sefl.lam2 * torch.mean(torch.sum(torch.abs(y_pred['X2_recon'] - y_pred['X2']), 1))
+                return loss
+
         return Loss()
 
+    def get_optim(self):
+        class Opt:
+            def __init__(self):
+                self.closure_bool = 0 
+                self.optD1 = RMSprop(self.D1.get_trainable(), self.params['lr'])
+                self.optD2 = RMSprop(self.D2.get_trainable(), self.params['lr'])
+                self.optG =  RMSprop(self.G1.get_trainable() + self.G2.get_trainable(), self.params['lr'])
 
+            def zero_grad(self):
+                self.optD1.zero_grad()
+                self.optD2.zero_grad()
+                self.optG.zero_grad()
 
+            def step(self):
+                self.optD1.step()
+                self.optD2.step()
+                self.optG.step()
+            
+            def call(self, context):
+                pass
+        self.opt = Opt()
+        return self.opt
